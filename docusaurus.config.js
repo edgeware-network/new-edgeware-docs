90b94f4fa7390414e054fc9dc4397155d303128a
Adam <44712760+Depth-Hoar@users.noreply.github.com>
2022-04-12 16:28:24 -0600

    side bar build day1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,11 +25,80 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
+
+// /** @type {import('@docusaurus/types').Config} */
+// const config = {
+//   title: 'Edgeware',
+//   tagline: 'Dinosaurs are cool',
+//   url: 'https://edgewa.re/',
+//   baseUrl: '/',
+//   onBrokenLinks: 'throw',
+//   onBrokenMarkdownLinks: 'warn',
+//   favicon: 'img/edgeware-main-logomark.svg',
+//   organizationName: 'edgeware-network', // Usually your GitHub org/user name.
+//   projectName: 'edgeware', // Usually your repo name.
+
+//   presets: [
+//     [
+//       'classic',
+//       /** @type {import('@docusaurus/preset-classic').Options} */
+//       ({
+//         docs: {
+//           routeBasePath: '/',//require.resolve('./sidebars.js'),
+//           // Please change this to your repo.
+//           editUrl: 'https://github.com/edgeware-network/new-edgeware-docs',
+//         },
+//         blog: false,
+//         // {
+//         //   showReadingTime: true,
+//         //   // Please change this to your repo.
+//         //   editUrl:
+//         //     'https://github.com/edgeware-network/new-edgeware-docs',
+//         // },
+//         theme: {
+//           customCss: require.resolve('./src/css/custom.css'),
+//         },
+//       }),
+//     ],
+//   ],
+
+//   themeConfig:
+//     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+//     ({
+//       navbar: {
+//         // title: 'Edgeware',
+//         logo: {
+//           alt: 'My Site Logo',
+//           src: 'img/edgeware-main-logomark.svg',
+//         },
+//         items: [
+//           {
+//             type: 'doc',
+//             docId: 'welcome/welcome',
+//             position: 'left',
+//             label: 'Docs',
+//           },
+//           // {to: '/blog', label: 'Blog', position: 'left'},
+//           {
+//             href: 'https://github.com/edgeware-network/new-edgeware-docs',
+//             label: 'GitHub',
+//             position: 'right',
+//           },
+//         ],
+//       },
+//       prism: {
+//         theme: lightCodeTheme,
+//         darkTheme: darkCodeTheme,
+//       },
+//     }),
+// };
+
+// module.exports = config;
+
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
+module.exports = {
   title: 'Edgeware',
   tagline: 'Dinosaurs are cool',
   url: 'https://edgewa.re/',
@@ -42,11 +111,11 @@ const config = {
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      '@docusaurus/preset-classic',
       ({
         docs: {
-          routeBasePath: '/', //require.resolve('./sidebars.js'),
+          routeBasePath: '/',//require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/edgeware-network/new-edgeware-docs',
         },
@@ -63,7 +132,6 @@ const config = {
       }),
     ],
   ],
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -88,56 +156,9 @@ const config = {
           },
         ],
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Docs',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
-};
-
-module.exports = config;
+}
